6399e40137415c7218f324de0dfcf5676ccf8c9f
matthieu <sixertoy.github@gmail.com>
2022-04-07 19:58:52 +0200

    update documentation

--- web/README.md
+++ web/README.md
@@ -1,6 +1,10 @@
 ## Docker
 
 ```bash
+yarn build
 docker build -t super/hero .
 docker run -it -p 8080:80 super/hero
+dk-bash-last
+chown -R `whoami`:www-data uploads
+# http://localhost:8080/
 ```
--- web/public/upload.php
+++ web/public/upload.php
@@ -14,7 +14,22 @@ if (!isset($_FILES['file'])) {
 $id = time();
 $outputfile = './uploads/'.$id.'.csv';
 $postfilename = $_FILES['file']['tmp_name'];
+
+if (!is_dir(dirname($outputfile))) {
+  send_response(false, 'Output folder do not exists');
+  exit;
+}
+
+if (!is_writable(dirname($outputfile))) {
+  send_response(false, 'Output folder is not writable');
+  exit;
+}
+
+if (!isset($postfilename)) {
+  send_response(false, 'Tmp file do not exists');
+  exit;
+}
 move_uploaded_file($postfilename, $outputfile);
-sleep(1.5);
+// sleep(1.5);
 send_response($id, false);
 exit;
